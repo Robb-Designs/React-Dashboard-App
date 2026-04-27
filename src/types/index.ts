481687d542b1b7186e241d80ed26750b051f0372
Robb-Designs <rtlee@aggies.ncat.edu>
2026-04-27 14:33:23 -0400

    Add TaskItemProps interface with task and action handlers

--- src/types/index.ts
+++ src/types/index.ts
@@ -1,15 +1,24 @@
 // TYPES----------------------------------------------------------------------------------------------------------------
 
 
-// TASK TYPE
+// TASK DATA STRUCTURE--------------------------------------------------------------------------------------------------
 //Reusable variable for task filtering
 export type Status = "inProgress" | "overdue" | "complete"
 
-//Structured the data for Task Component
+//Structured the data for every Task
 export interface Task {
-  id: string;
+  id: string; //For a unique identifier *used for updates, deletes*
   status: Status;
   title: string;
-  description?: string;
+  description?: string; //Optional
   date: string;
+}
+
+// TASK COMPONENT PROPS--------------------------------------------------------------------------------------------------
+
+//Task Item
+export interface TaskItemProps {
+    task: Task; //receives a full Task object that follows the Task interface. (Now in my component I can do things like 'task.title')
+    onDelete: (id: string) => void;
+    onToggle: (id: string) => void
 }
